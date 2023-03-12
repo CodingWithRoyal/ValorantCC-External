c719ac9a01093205519d363faedfee8b561ceb24
CodingWithRoyal <iamroyalrandhawa@gmail.com>
2023-03-12 11:28:04 +0530

    v2.0 finally done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,13 +11,16 @@ namespace ValorantCC_Custom_WPF
     public partial class MainWindow : Window
     {
         private NotifyIcon _notifyIcon;
-        private string _crosshair = "Circle"; // cricle / plus
-        private int _crosshairSize = 10;
-        private System.Windows.Media.SolidColorBrush _crosshairColor = System.Windows.Media.Brushes.Red;
-        private string _crosshairColorString = "Red";
+        private System.Windows.Media.SolidColorBrush _crosshairColor;
 
         private static string[] crosshairs = new string[] { "Circle", "Plus" };
         private static string[] colors = new string[] { "White", "Black", "Red", "Cyan", "Lime", "Navy", "Orange", "Tomato", "Orchid", "Magenta" };
+
+        // Read user settings or load default values
+        private string _crosshair = Properties.Settings.Default.user_crosshair; // cricle / plus
+        private int _crosshairSize = Properties.Settings.Default.user_crosshairSize;
+        private int _crosshairThickness = Properties.Settings.Default.user_crosshairThickness;
+        private string _crosshairColorString = Properties.Settings.Default.user_crosshairColor;
 
         public MainWindow()
         {
@@ -37,17 +40,6 @@ namespace ValorantCC_Custom_WPF
             _notifyIcon.Visible = true;
             // Create the context menu for the notify icon
             ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
-            
-            ToolStripMenuItem sizeMenuItem = new ToolStripMenuItem("Crosshair Size");
-            contextMenuStrip.Items.Add(sizeMenuItem);
-
-            for (int i = 5; i <= 25; i += 5)
-            {
-                ToolStripMenuItem sizeItem = new ToolStripMenuItem(i.ToString() + "px");
-                sizeItem.Checked = i == _crosshairSize;
-                sizeItem.Click += SizeItem_Click;
-                sizeMenuItem.DropDownItems.Add(sizeItem);
-            }
 
             ToolStripMenuItem typeMenuItem = new ToolStripMenuItem("Crosshair Type");
             contextMenuStrip.Items.Add(typeMenuItem);
@@ -57,6 +49,26 @@ namespace ValorantCC_Custom_WPF
                 typeItem.Checked = crosshairs[i] == _crosshair;
                 typeItem.Click += TypeItem_Click;
                 typeMenuItem.DropDownItems.Add(typeItem);
+            }
+
+            ToolStripMenuItem thicknessMenuItem = new ToolStripMenuItem("Crosshair Thickness");
+            contextMenuStrip.Items.Add(thicknessMenuItem);
+            for (int i = 1; i <= 10; i += 1)
+            {
+                ToolStripMenuItem thicknessItem = new ToolStripMenuItem(i.ToString() + "px");
+                thicknessItem.Checked = i == _crosshairSize;
+                thicknessItem.Click += ThicknessItem_Click;
+                thicknessMenuItem.DropDownItems.Add(thicknessItem);
+            }
+
+            ToolStripMenuItem sizeMenuItem = new ToolStripMenuItem("Crosshair Size");
+            contextMenuStrip.Items.Add(sizeMenuItem);
+            for (int i = 4; i <= 40; i += 4)
+            {
+                ToolStripMenuItem sizeItem = new ToolStripMenuItem(i.ToString() + "px");
+                sizeItem.Checked = i == _crosshairSize;
+                sizeItem.Click += SizeItem_Click;
+                sizeMenuItem.DropDownItems.Add(sizeItem);
             }
 
             ToolStripMenuItem colorMenuItem = new ToolStripMenuItem("Crosshair Color");
@@ -95,7 +107,30 @@ namespace ValorantCC_Custom_WPF
             }
             clickedItem.Checked = true;
             _crosshairSize = int.Parse(clickedItem.Text.Replace("px", ""));
+            
+            // Save user settings
+            Properties.Settings.Default.user_crosshairSize = _crosshairSize;
+            Properties.Settings.Default.Save();
+
             Debug.WriteLine("Crosshair Size Changed:" + clickedItem.Text);
+            DrawCrosshair();
+        }
+
+        private void ThicknessItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem clickedItem = (ToolStripMenuItem)sender;
+            foreach (ToolStripMenuItem item in clickedItem.Owner.Items)
+            {
+                item.Checked = false;
+            }
+            clickedItem.Checked = true;
+            _crosshairThickness = int.Parse(clickedItem.Text.Replace("px", ""));
+
+            // Save user settings
+            Properties.Settings.Default.user_crosshairThickness = _crosshairThickness;
+            Properties.Settings.Default.Save();
+
+            Debug.WriteLine("Crosshair Thickness Changed:" + clickedItem.Text);
             DrawCrosshair();
         }
 
@@ -108,22 +143,18 @@ namespace ValorantCC_Custom_WPF
             }
             clickedItem.Checked = true;
             _crosshair = clickedItem.Text;
+
+            // Save user settings
+            Properties.Settings.Default.user_crosshair = _crosshair;
+            Properties.Settings.Default.Save();
+
             Debug.WriteLine("Crosshair Type Changed:" + clickedItem.Text);
             DrawCrosshair();
         }
 
-        private void ColorItem_Click(object sender, EventArgs e)
-        {
-            ToolStripMenuItem clickedItem = (ToolStripMenuItem)sender;
-            foreach (ToolStripMenuItem item in clickedItem.Owner.Items)
+        private void updateCrosshairColor() {
+            switch (_crosshairColorString)
             {
-                item.Checked = false;
-            }
-            clickedItem.Checked = true;
-            _crosshairColorString = clickedItem.Text;
-            Debug.WriteLine("Crosshair Color Changed:" + clickedItem.Text);
-
-            switch (_crosshairColorString) {
                 case "Red":
                     _crosshairColor = System.Windows.Media.Brushes.Red;
                     break;
@@ -155,6 +186,23 @@ namespace ValorantCC_Custom_WPF
                     _crosshairColor = System.Windows.Media.Brushes.Cyan;
                     break;
             }
+        }
+
+        private void ColorItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem clickedItem = (ToolStripMenuItem)sender;
+            foreach (ToolStripMenuItem item in clickedItem.Owner.Items)
+            {
+                item.Checked = false;
+            }
+            clickedItem.Checked = true;
+            _crosshairColorString = clickedItem.Text;
+
+            // Save user settings
+            Properties.Settings.Default.user_crosshairColor = _crosshairColorString;
+            Properties.Settings.Default.Save();
+
+            Debug.WriteLine("Crosshair Color Changed:" + clickedItem.Text);
 
             DrawCrosshair();
         }
@@ -162,6 +210,8 @@ namespace ValorantCC_Custom_WPF
         private void DrawCrosshair() {
             Debug.WriteLine("Crosshair Type:" + _crosshair);
             Debug.WriteLine("Crosshair Size:" + _crosshairSize);
+            
+            updateCrosshairColor(); // update brush color
             switch (_crosshair) {
                 case "Circle":
                     DrawCircle();
@@ -178,7 +228,7 @@ namespace ValorantCC_Custom_WPF
             canvas.Children.Clear();
             Ellipse circle = new Ellipse();
             circle.Stroke = _crosshairColor;
-            circle.StrokeThickness = 2;
+            circle.StrokeThickness = _crosshairThickness;
             circle.Width = _crosshairSize;
             circle.Height = _crosshairSize;
             double left = (SystemParameters.PrimaryScreenWidth - _crosshairSize) / 2;
@@ -196,7 +246,7 @@ namespace ValorantCC_Custom_WPF
         {
             Debug.WriteLine("Drawing Plus");
             canvas.Children.Clear();
-            double thickness = Math.Max(_crosshairSize / 10, 1);
+            double thickness = _crosshairThickness; // Math.Max(_crosshairSize / 10, 1);
             double halfWidth = _crosshairSize / 2;
             double halfThickness = thickness / 2;
             Rect verticalRect = new Rect(halfWidth - halfThickness, 0, thickness, _crosshairSize);
